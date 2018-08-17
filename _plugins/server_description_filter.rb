64d39d339132c91391ea9627adb0de0c981c7846
Tom Hughes <tom@compton.nu>
2018-08-17 10:35:53 +0100

    Fix exception when a server has no roles

--- _plugins/server_description_filter.rb
+++ _plugins/server_description_filter.rb
@@ -2,7 +2,7 @@ module Jekyll
   module ServerDescriptionFilter
     def server_description(roles)
       descriptions = @context.registers[:site].data['descriptions']
-      roles.map { |role| descriptions[role] }.compact.join('<br>')
+      Array(roles).map { |role| descriptions[role] }.compact.join('<br>')
     end
   end
 end
